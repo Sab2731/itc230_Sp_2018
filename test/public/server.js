d3b0532e8aa080ee95635cf45e0e695752a01d55
Sab2731 <t1010_2731@hotmail.com>
2018-04-18 21:06:20 -0700

    new request
    add switch method

--- test/public/server.js
+++ test/public/server.js
@@ -1,8 +1,41 @@
 var http = require("http"); 
-
-var fs = require("fs");
-fs.readFile('index.html', function (err, data) {
-   if (err) return console.error(err);
-   console.log(data.toString());
-}).listen(8000);
-console.log('Listening on port 8000');
+http.createServer(function(req,res) {
+  var path = req.url.toLowerCase();
+  
+  
+  switch(path) {
+    
+    case '/':
+    case '/index':
+    case '/index.html': 
+      var fs = require("fs");
+      fs.readFile('index.html', function (err, data) {
+     if (err) return console.error(err);
+     console.log(data.toString());
+     res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(data);
+      });
+      break;
+      
+    case '/about':
+    case '/about.html':
+       var fs = require("fs");
+      fs.readFile('about.html', function (err, data) {
+     if (err) return console.error(err);
+     console.log(data.toString());
+     res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(data);
+      });
+      break;
+      
+    default:
+      var fs = require("fs");
+      fs.readFile('pagenotfound.html', function (err, data) {
+     if (err) return console.error(err);
+     console.log(data.toString());
+     res.writeHead(404, {'Content-Type': 'text/html'});
+      res.end(data);
+      });
+      break;
+    }
+}).listen(process.env.PORT || 3000);
